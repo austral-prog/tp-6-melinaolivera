91e382642b7620f37502698e052856425d57eabe
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-05-10 12:38:53 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,13 @@
-# Replace the "ANSWER HERE" with your answer
+def remove_elements(lista):
+    if len(lista) >=6:
+        del lista[5]
+        del lista[4]
+        del lista[0]
+    elif len(lista) >=5:
+        del lista[0]
+        del lista[4]
+    else:
+        del lista[0]
+    print(lista)
 
-def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+remove_elements(['red','green','white','black','pink','yellow'])
